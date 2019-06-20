0c3f9e7a7362c3690cd6c84aab4fd8c1b13e038f
Ilham Dedi <muhammadilhamdedi@gmail.com>
2019-06-20 20:16:52 +0800

    add login page

--- lib/config/config.go
+++ lib/config/config.go
@@ -6,6 +6,10 @@ import (
 )
 
 var (
+	// Running Environment
+	EnvironmentMode = os.Getenv("ENV_MODE")
+
+	// Internal Endpoint
 	AincradEndpoint    = os.Getenv("AINCRAD_ENDPOINT")
 	KiritoEndpoint     = os.Getenv("KIRITO_ENDPOINT")
 	HeathcliffEndpoint = os.Getenv("HEATHCLIFF_ENDPOINT")
@@ -13,9 +17,18 @@ var (
 	YuiEndpoint        = os.Getenv("YUI_ENDPOINT")
 	AsunaEndpoint      = os.Getenv("ASUNA_ENDPOINT")
 	SinonEndpoint      = os.Getenv("SINON_ENDPOINT")
+
+	// Received Root Path
+	AincradRootPath    = os.Getenv("AINCRAD_ROOT_PATH")
+	KiritoRootPath     = os.Getenv("KIRITO_ROOT_PATH")
+	HeathcliffRootPath = os.Getenv("HEATHCLIFF_ROOT_PATH")
+	SterbenRootPath    = os.Getenv("STERBEN_ROOT_PATH")
+	YuiRootPath        = os.Getenv("YUI_ROOT_PATH")
+	AsunaRootPath      = os.Getenv("ASUNA_ROOT_PATH")
+	SinonRootPath      = os.Getenv("SINON_ROOT_PATH")
 )
 
 func init() {
 	// Log used configurations here
-	fmt.Printf("Aincrad is listening on %s\n...", AincradEndpoint)
+	fmt.Printf("Running %s Environment...\n", EnvironmentMode)
 }
