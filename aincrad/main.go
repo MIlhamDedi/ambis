676257f25e6a1ad095cdc041471e647994f04602
Ilham Dedi <muhammadilhamdedi@gmail.com>
2019-06-22 16:46:14 +0800

    implement rooting with aincrad

--- aincrad/main.go
+++ aincrad/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"ambis/aincrad/proxy"
 )
 
-func main(){
+func main() {
 	fmt.Println("This is Aincrad")
+
+	proxyHandler := proxy.NewHandler()
+	http.Handle("/", proxyHandler)
+
+	fmt.Println("Aincrad is serving on port 5000...")
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
